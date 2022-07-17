9502b99427f97baf06411f24eda55dfc1b2193dc
Tassak <damian@mythe.pl>
2022-07-18 00:00:01 +0200

    Adding vue-i18n

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -14,32 +14,33 @@ function goToHome() {
 </script>
 
 <template>
-  <div class="about">
-    <h1>About boilerplate</h1>
-    <span @click="goToHome" class="about__go-to-home">Go Back to Home</span>
-    <p>Current count: {{ counterStore.count }}</p>
-    <button type="button" @click="increment">Add</button>
-    <ul>
-      <li>Vite + TS</li>
-      <li>Pinia</li>
-      <li>Router</li>
-      <li>SASS</li>
-      <li>Vitest + testing-library</li>
-    </ul>
-  </div>
+	<div class="about">
+		<h1>About boilerplate</h1>
+		<span @click="goToHome" class="about__go-to-home">Go Back to Home</span>
+		<p>Current count: {{ counterStore.count }}</p>
+		<button type="button" @click="increment">Add</button>
+		<ul>
+			<li>Vite + TS</li>
+			<li>Pinia</li>
+			<li>Router</li>
+			<li>SASS</li>
+			<li>Vitest + testing-library</li>
+			<li>{{ $t('message.i18n') }}</li>
+		</ul>
+	</div>
 </template>
 
 <style lang="scss" scoped>
 .about {
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  &__go-to-home {
-    text-decoration: underline;
-    color: #42b983;
-    cursor: pointer;
-  }
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	&__go-to-home {
+		text-decoration: underline;
+		color: #42b983;
+		cursor: pointer;
+	}
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,28 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { createI18n, I18nOptions } from 'vue-i18n';
 import router from './router/index';
 import App from './App.vue';
+import enUS from './translations/en-US.json';
 
+// store
 const pinia = createPinia();
+
+// locales
+type MessageSchema = typeof enUS;
+
+const i18n = createI18n<I18nOptions, [MessageSchema], 'en-US'>({
+	locale: 'en-US',
+	fallbackLocale: 'en-US',
+	messages: {
+		'en-US': enUS,
+	},
+});
+
+// app
 const app = createApp(App);
 
+app.use(i18n);
 app.use(pinia);
 app.use(router);
 app.mount('#app');
